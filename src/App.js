909e4a95f28783a188797b95ecb6a59303c00a3d
Rostvor <rostvorgame@gmail.com>
2021-08-29 22:22:46 +0300

    second push

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [items, setItems] = useState(null);
   const [url, setUrl] = useState("https://rickandmortyapi.com/api/character?page=");
   const [gender, setGender] = useState("");
- 
+  
   useEffect(
     () => {
       fetch(url).then((respon) => respon.json()).then((results) => { setItems(results) });
@@ -28,22 +28,24 @@ function App() {
     }
   }
   function search(names) {
-    if(gender ==='male'){
+    if (gender === 'male') {
       return names.filter(person => person.gender === "Male")
-    }else if(gender==='female'){
+    } else if (gender === 'female') {
       return names.filter(person => person.gender === "Female")
-    }else  return names
+    } else return names
   }
 
   return (
     <div className="App">
-      <input type="radio" name="gender" value='all' onChange={(e)=>setGender(e.target.value)} /> <label>All</label>
-      <input type="radio" name="gender" value='male' onChange={(e)=>setGender(e.target.value)} /> <label>Male</label>
-      <input type="radio" name="gender" value='female' onChange={(e)=>setGender(e.target.value)} /> <label>FeMale</label>
-      
+      <input type="radio" name="gender" value='all' onChange={(e) => setGender(e.target.value)} /> <label>All</label>
+      <input type="radio" name="gender" value='male' onChange={(e) => setGender(e.target.value)} /> <label>Male</label>
+      <input type="radio" name="gender" value='female' onChange={(e) => setGender(e.target.value)} /> <label>FeMale</label>
+
       {items && <Characters persone={search(items.results)} />}
-      <button onClick={() => next()}>Next</button>
       <button onClick={() => prev()}>Prev</button>
+      <button onClick={() => next()}>Next</button>
+      
+      
     </div>
   );
 }
--- src/component/Characters.js
+++ src/component/Characters.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react'
-
+import Info from './Info';
 
 
 
 export default function Characters({ persone }) {
+
+    const [show, setShow] = useState(false);
+
+    function changeShow() {
+        setShow(!show);
+    }
+
     return (
         <ul>
             {persone.map((item) => (
-                < li key={item.id} > {item.name}</li>
+                < li key={item.id} onClick={() => changeShow()}>{item.name}{show ? <Info props={item}/> : 'bye'} </li>
             ))}
+         
         </ul >
     )
 }
